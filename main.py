9900153fd095a4fb2a88b061a87807f872241a43
Injenus <injenus777@gmail.com>
2021-10-01 23:34:53 +0300

    #1.1 - classes

--- main.py
+++ main.py
@@ -8,28 +8,32 @@ class Window(QMainWindow):  #create class
     def __init__(self):     #create констурктор//настройка класса
         super(Window, self).__init__()
 
+        self.setWindowTitle("Простая прогрмма")
+        self.setGeometry(300, 250, 350, 200)
 
-def add_label():    #обработчик нажатия кнопки
-    print("add")
+        self.new_text = QtWidgets.QLabel(self)
 
+        self.main_text = QtWidgets.QLabel(self)
+        self.main_text.setText("Это базовая надпись")
+        self.main_text.move(100, 100)
+        self.main_text.adjustSize()
+
+        self.btn = QtWidgets.QPushButton(self)
+        self.btn.move(70, 150)
+        self.btn.setText("Нажми на меня")
+        self.btn.setFixedWidth(200)
+        self.btn.clicked.connect(self.add_label)
+
+
+    def add_label(self):    #обработчик нажатия кнопки
+        #self.new_text.SetText("Вторая надпись")
+        #self.new_text.move(100, 50)
+        #self.new_text.adjustSize()
+        print('dgf')
 
 def application():
     app = QApplication(sys.argv)
-    window = QMainWindow()
-
-    window.setWindowTitle("Простая прогрмма")
-    window.setGeometry(300, 250, 350, 200)
-
-    main_text = QtWidgets.QLabel(window)
-    main_text.setText("Это базовая надпись")
-    main_text.move(100, 100)
-    main_text.adjustSize()
-
-    btn=QtWidgets.QPushButton(window)
-    btn.move(70, 150)
-    btn.setText("Нажми на меня")
-    btn.setFixedWidth(200)
-    btn.clicked.connect(add_label)
+    window = Window()
 
     window.show()
     sys.exit(app.exec_())
